9f4e4ccee19fb15c771abed2520e9dd485aeda85
Vitor <vitor.reis@crmservices.com.br>
2024-02-05 16:45:42 -0300

    feat: cancelation confirm

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -59,15 +59,19 @@ export default async function Home() {
       </div>
 
       <div className="mt-6">
-        <h2 className="px-5 text-xs uppercase text-gray-400 font-bold mb-3">
-          Recomendados
-        </h2>
+        {confirmedBookings.length > 0 && (
+          <>
+            <h2 className="px-5 text-xs uppercase text-gray-400 font-bold mb-3">
+              Recomendados
+            </h2>
 
-        <div className="flex px-5 gap-4 overflow-x-auto [&::webkit-scrollbar]:hidden">
-          {barbershops.map((barbershop) => (
-            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
-          ))}
-        </div>
+            <div className="flex px-5 gap-4 overflow-x-auto [&::webkit-scrollbar]:hidden">
+              {barbershops.map((barbershop) => (
+                <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+              ))}
+            </div>
+          </>
+        )}
       </div>
 
       <div className="mt-6 mb-[4.5rem]">
